4ecbb32acbc7b73cfe52f9dd5128dbd3c87b7720
PieSignal <leeon@insiro.me>
2020-07-01 18:51:54 +0900

    fix: wrong type

--- src/sender/Setup/role.ts
+++ src/sender/Setup/role.ts
@@ -2,17 +2,17 @@ import { Guild } from 'discord.js';
 import { getRepository } from 'typeorm';
 export const setRole = async (
     guild: Guild,
-    args: Array<String>
+    args: Array<string>
 ): Promise<string> => {
     const roleName = args.join(' ');
     const role = guild.roles.cache.find((role) => role.name === roleName);
-    if (role == undefined) return 'Wrong Role Name';
+    if (role === undefined) return 'Wrong Role Name';
     const repo = getRepository('server');
     await repo
         .createQueryBuilder()
         .update()
         .set({ role: role.id })
-        .where('serverId = :gID', { gID: guild.id!.toString() })
+        .where('serverId = :gID', { gID: guild.id.toString() })
         .execute()
         .catch(() => {
             return 'failed to set respose Role ' + roleName;
