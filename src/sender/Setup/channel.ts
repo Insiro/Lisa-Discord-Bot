8a9e19ca31f477188772e674f7acfbfa53ceef5f
PieSignal <leeon@insiro.me>
2020-07-01 18:09:33 +0900

    add: channel Settings

--- src/sender/Setup/channel.ts
+++ src/sender/Setup/channel.ts
@@ -1,17 +1,34 @@
 import { getRepository } from 'typeorm';
+import { Guild } from 'discord.js';
 export const setResposeChannel = async (
-    guildID: string,
+    guild: Guild,
     channelID: string
 ): Promise<string> => {
+    const channel = guild.channels.cache.get(channelID);
+    if (channel === undefined) return 'failed to set respose Channel';
     const repo = getRepository('server');
     await repo
         .createQueryBuilder()
         .update()
         .set({ channel: channelID })
-        .where('serverId = :gID', { gID: guildID.toString() })
+        .where('serverId = :gID', { gID: guild.id.toString() })
         .execute()
         .catch(() => {
             return 'failed to set respose Channel';
         });
-    return 'changed respose Channel to';
+    return 'changed respose Channel to ' + channel.name;
+};
+export const resetChannel = async (guild: Guild | null): Promise<string> => {
+    if (guild == null) return 'Not in Server';
+    const repo = getRepository('server');
+    await repo
+        .createQueryBuilder()
+        .update()
+        .set({ channel: null })
+        .where('serverId = :gID', { gID: guild.id.toString() })
+        .execute()
+        .catch(() => {
+            return 'failed to set respose Channel\nPlz rejoin Bot';
+        });
+    return 'success to reset';
 };
--- src/sender/Setup/index.ts
+++ src/sender/Setup/index.ts
@@ -4,19 +4,24 @@ import { setClan } from './clan';
 import { setPrefix } from './prefix';
 import { getRepository } from 'typeorm';
 import { Server } from '../../entity/Server';
+import { setResposeChannel, resetChannel } from './channel';
 
 const hasPermission = async (msg: Message): Promise<boolean> => {
     if (
-        msg.guild !== null &&
-        msg.member !== null &&
-        msg.member!.hasPermission('ADMINISTRATOR') == true
+        msg.guild === null &&
+        msg.member === null &&
+        !msg.member!.hasPermission('ADMINISTRATOR')
     )
         return true;
     const info: Server = (await getRepository('server')
         .createQueryBuilder()
         .where('serverId =:sID', { sID: msg.guild!.id.toString() })
         .getOne()) as Server;
-    if (info.role !== null && msg.member!.roles.cache.has(info.role!))
+    if (
+        info.role !== null &&
+        msg.member!.roles.cache.has(info.role!) &&
+        (info.channel === null || info.channel === msg.channel.id.toString())
+    )
         return true;
     return false;
 };
@@ -26,10 +31,13 @@ export const setup = async (
     args: Array<string>
 ): Promise<string> => {
     let outStr = "haven't permision";
+    if (args[0] === '채널초기화' || args[0] === 'resetChannel')
+        return resetChannel(msg.guild);
     if (!(await hasPermission(msg))) return "haven't permission";
     switch (args[0]) {
         case '채널':
         case 'channel':
+            outStr = await setResposeChannel(msg.guild!, args[1]);
             break;
         case '클랜주소':
         case 'clanLink':
