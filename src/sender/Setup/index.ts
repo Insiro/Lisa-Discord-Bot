0dfcdce6a0a819cf10726fa832629723b2f0d496
PieSignal <leeon@insiro.me>
2020-07-01 17:28:52 +0900

    add : change response Role

--- src/sender/Setup/index.ts
+++ src/sender/Setup/index.ts
@@ -2,13 +2,31 @@ import { Message } from 'discord.js';
 import { setRole } from './role';
 import { setClan } from './clan';
 import { setPrefix } from './prefix';
+import { getRepository } from 'typeorm';
+import { Server } from '../../entity/Server';
+
+const hasPermission = async (msg: Message): Promise<boolean> => {
+    if (
+        msg.guild !== null &&
+        msg.member !== null &&
+        msg.member!.hasPermission('ADMINISTRATOR') == true
+    )
+        return true;
+    const info: Server = (await getRepository('server')
+        .createQueryBuilder()
+        .where('serverId =:sID', { sID: msg.guild!.id.toString() })
+        .getOne()) as Server;
+    if (info.role !== null && msg.member!.roles.cache.has(info.role!))
+        return true;
+    return false;
+};
+
 export const setup = async (
     msg: Message,
     args: Array<string>
 ): Promise<string> => {
-    let outStr = 'will be support';
-    if (msg.guild === null) outStr = 'not in Server';
-
+    let outStr = "haven't permision";
+    if (!(await hasPermission(msg))) return "haven't permission";
     switch (args[0]) {
         case '채널':
         case 'channel':
@@ -16,6 +34,10 @@ export const setup = async (
         case '클랜주소':
         case 'clanLink':
             outStr = await setClan(msg.guild!.id.toString(), args[1]);
+            break;
+        case 'role':
+        case '역할':
+            outStr = await setRole(msg.guild!, args.slice(1));
             break;
         default:
             outStr = 'Wrong Command';
--- src/sender/Setup/role.ts
+++ src/sender/Setup/role.ts
@@ -1,17 +1,21 @@
+import { Guild } from 'discord.js';
 import { getRepository } from 'typeorm';
 export const setRole = async (
-    guildID: string,
-    role: string
+    guild: Guild,
+    args: Array<String>
 ): Promise<string> => {
+    const roleName = args.join(' ');
+    const role = guild.roles.cache.find((role) => role.name === roleName);
+    if (role == undefined) return 'Wrong Role Name';
     const repo = getRepository('server');
     await repo
         .createQueryBuilder()
         .update()
-        .set({ role: role })
-        .where('serverId = :gID', { gID: guildID.toString() })
+        .set({ role: role.id })
+        .where('serverId = :gID', { gID: guild.id!.toString() })
         .execute()
         .catch(() => {
-            return 'failed to set respose Role';
+            return 'failed to set respose Role ' + roleName;
         });
-    return 'changed respose Role to';
+    return 'changed respose Role to ' + role.name;
 };
