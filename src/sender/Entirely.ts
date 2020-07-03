11f29bd89e0cc35dd7eaaa99e9e7f4dcfc5a9049
PieSignal <leeon@insiro.me>
2020-07-03 18:11:51 +0900

    style : sending msg format

--- src/sender/Entirely.ts
+++ src/sender/Entirely.ts
@@ -19,12 +19,18 @@ const getPlayerID = async (playerName: string): Promise<string> => {
     }
 };
 const recordStr = (recordJson: any): string => {
+    const winRate =
+        (parseInt(recordJson[0]['winCount']) * 100) /
+        (parseInt(recordJson[0]['winCount']) +
+            parseInt(recordJson[0]['loseCount']));
     return (
         '- 공식  |  ' +
         recordJson[0]['winCount'] +
         '승  |  ' +
         recordJson[0]['loseCount'] +
-        '패\n' +
+        '패  |  ' +
+        winRate.toFixed(1).toString() +
+        '%\n' +
         '- 일반  |  ' +
         recordJson[1]['winCount'] +
         '승  |  ' +
@@ -53,6 +59,12 @@ export const getEntirely = async (
     playerName: string,
     gameTypeId = 'rating'
 ): Promise<string> => {
+    let gameType: string;
+    if (gameTypeId === 'normal') gameType = '일반';
+    else {
+        gameTypeId = 'rating';
+        gameType = '공식';
+    }
     const playerID = await getPlayerID(playerName);
     if (playerID === parseErrorMsg) return parseErrorMsg;
     try {
@@ -66,15 +78,17 @@ export const getEntirely = async (
         const result: string = await request.get(options);
         const json = JSON.parse(result);
         return (
-            '```markdown\n' +
+            '```' +
             json['nickname'] +
             '\t|\t' +
             json['tierName'] +
             '\t|\t최대RP\t' +
             json['maxRatingPoint'] +
-            '\n------------------------\n' +
+            '```\n```' +
             recordStr(json['records']) +
-            '\n------------------------\n' +
+            '\n```\n```최근 ' +
+            gameType +
+            ' 전적\n' +
             matchStr(json['matches']['rows']) +
             '```'
         );
