42547d2689e449d5d9faba955f2fa68c3f64ede2
Insiro <leeon@insiro.me>
2022-07-25 18:14:24 +0900

    migrate: chage main functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Client } from 'discord.js';
-import { DiscordApiKey, prefix } from './config';
-import 'reflect-metadata';
+import { Client, Intents } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { DiscordApiKey, DiscordAppId, prefix } from './config';
 import schedule from 'node-schedule';
 
 import AppDataSource from './data-sources';
@@ -8,18 +8,15 @@ import * as guildInit from './utils/guild';
 import { sender } from './sender';
 import { worker } from './newsUpdater';
 
+import { Routes } from 'discord-api-types/v10';
+
+
 async function main(): Promise<void> {
-    const client = new Client();
-    client.on('ready', () => {
-        client.user?.setPresence({
-            status: 'online',
-            activity: {
-                type: 'LISTENING',
-                name: prefix + 'help',
-            },
-        });
+    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    client.on('interactionCreate', async (interaction) => {
+        if (interaction.isCommand()) sender(interaction);
     });
-    client.on('message', (msg): void => void sender(msg));
+    // client.on('message', (msg): void => void sender(msg));
     client.on(
         'guildCreate',
         (guild): void => void guildInit.registerGuild(guild)
@@ -29,13 +26,17 @@ async function main(): Promise<void> {
         (guild): void => void guildInit.deleteGuild(guild)
     );
     await AppDataSource.initialize();
-    schedule.scheduleJob('0 30 * * * *', () => {
-        void worker(client);
-    });
-    schedule.scheduleJob('0 00 * * * *', () => {
-        void worker(client);
-    });
+    // schedule.scheduleJob('0 30 * * * *', () => {
+    //     void worker(client);
+    // });
+    // schedule.scheduleJob('0 00 * * * *', () => {
+    //     void worker(client);
+    // });
+
     await client.login(DiscordApiKey);
+
+    client.user.setActivity('/help 테스팅', { type: 'LISTENING' });
+
     console.log('start CyphersDiscord Bot!!');
 }
 main().catch((error) => console.log(error));
--- src/sender/index.ts
+++ src/sender/index.ts
@@ -1,7 +1,10 @@
 import { getEntirely } from './Entirely';
 import { getMatchInfo } from './Match';
 import { help } from './Help';
-import { Message, MessageEmbed } from 'discord.js';
+import {
+    CommandInteraction,
+    MessageEmbed,
+} from 'discord.js';
 import { parse, SuccessfulParsedMessage } from 'discord-command-parser';
 import { getRanking } from './Ranking';
 import { setup } from './Setup';
@@ -10,54 +13,57 @@ import { BotServer } from '../entity/BotServer';
 import { getGuildInfo } from '../utils/guild';
 import { prefix } from '../config';
 const normalCommander = async (
-    msg: Message,
-    parsed: SuccessfulParsedMessage<Message>
+    interaction: CommandInteraction
 ): Promise<void> => {
-    const info: BotServer | null = await getGuildInfo(msg.guild);
+    const info: BotServer | null = await getGuildInfo(interaction.guild);
     if (
         info !== null &&
         info.channel !== null &&
         info.channel !== undefined &&
-        msg.channel.id.toString() !== info.channel
-    ) {
+        interaction.channelId !== info.channel
+    )
         return;
-    }
+
     let sendString: string | MessageEmbed;
-    switch (parsed.command) {
+    switch (interaction.commandName) {
         case '전적':
-            sendString = await getEntirely(parsed.arguments);
+        case 'record':
+            // sendString = await getEntirely(parsed.arguments);
             break;
         case '매치':
-            sendString = await getMatchInfo(parsed.arguments[0]);
+        case 'match':
+            // sendString = await getMatchInfo(parsed.arguments[0]);
             break;
-        case '클랜':
-            sendString = await clanController(msg, parsed.arguments);
+        case 'clan':
+            // sendString = await clanController(interaction);
             break;
         case '랭킹':
-            sendString = await getRanking(parsed.arguments[0]);
+        case 'ranking':
+            // sendString = await getRanking(interaction);
             break;
         default:
             return;
     }
-    msg.channel.send(sendString);
+    const msg =(sendString instanceof MessageEmbed)?  { embeds: [sendString] }: sendString
+    interaction.reply(msg);
 };
 
-export const sender = async (msg: Message): Promise<void> => {
-    const parsed = parse(msg, prefix);
-    if (!parsed.success) return;
+export const sender = async (
+    interaction: CommandInteraction
+): Promise<void> => {
     let sendString: string | MessageEmbed | null;
-    switch (parsed.command) {
+    switch (interaction.commandName) {
         case '설정':
-            sendString = await setup(msg, parsed.arguments);
+            // sendString = await setup(interaction.options);
             break;
         case 'help':
         case '도움말':
         case '명령어':
-            sendString = help(parsed.arguments);
+            // sendString = help(parsed.arguments);
             break;
         default:
-            normalCommander(msg, parsed);
-            return;
+            await normalCommander(interaction);
     }
-    if (sendString !== null) msg.channel.send(sendString);
+    // if (sendString instanceof MessageEmbed) msg.channel.send({ embeds:[sendString] })
+    // else if (sendString !== null) msg.channel.send(sendString);
 };
